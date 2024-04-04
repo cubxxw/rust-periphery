318c6082f6a998d7d60fffe8a19e837d481bf34c
Xinwei Xiong <3293172751NSS@gmail.com>
2024-04-04 18:52:17 +0800

    Update mod.rs

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,9 +1,12 @@
-// Helper macro to execute a system call that returns an `io::Result`.
+// Helper macro to execute a system call that returns an `io::Result`, providing more context in case of errors.
 macro_rules! syscall {
-    ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
+    ($fn:ident($($arg:expr),* $(,)*)) => {{
         let res = unsafe { libc::$fn($($arg, )*) };
         if res == -1 {
-            Err(std::io::Error::last_os_error())
+            Err(std::io::Error::new(
+                std::io::Error::last_os_error().kind(),
+                format!("{} failed with {:?}", stringify!($fn), ($($arg, )*)),
+            ))
         } else {
             Ok(res)
         }
